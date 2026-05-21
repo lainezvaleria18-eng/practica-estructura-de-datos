99d1109db66870ac4c20dde8e653495cf25bac91
MINEDUCYT <MINEDUCYT@YAMICOMPU>
2026-05-21 03:35:14 -0600

    Karla Yamileth - Ejercicio 6 finalizado: Generacion y visualizacion de la matriz transpuesta.

---
+++ src/Ejercicio6_Transpuesta.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Ejercicio6_Transpuesta {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        int[][] matriz = new int[3][3];
+
+        System.out.println("=== MATRIZ ORIGINAL ===");
+
+        // Llenar matriz
+        for (int f = 0; f < 3; f++) {
+            for (int c = 0; c < 3; c++) {
+
+                System.out.print("Numero [" + f + "][" + c + "]: ");
+                matriz[f][c] = entrada.nextInt();
+            }
+        }
+
+        System.out.println("\n=== MATRIZ TRANSPUESTA ===");
+
+        // Mostrar transpuesta
+        for (int c = 0; c < 3; c++) {
+            for (int f = 0; f < 3; f++) {
+
+                System.out.print(matriz[f][c] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
